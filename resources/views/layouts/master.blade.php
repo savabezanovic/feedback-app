346ca6e394c3c7cf62df811e8c76f8387acc41d3
Stefan Mihajlovic <stefan.mihajlovic@quantox.com>
2020-07-13 16:43:52 +0200

    Changed css for dashboard

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link rel="icon" href="{{ asset('images/F-Logo.jpg')}}">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -25,22 +26,21 @@
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
     </head>
-    <body>
-<div class="aside aside-media-view">
-    <div class="menu-media js-menu-media"><i class="fas fa-bars"></i></div>
-    <div class="web-name">
-        <div class="logo">F</div>
-        <span class="logo-name">FEEDBACK
-            <br>
-        APP</span>
-    </div>
-    @yield('users')
-</div>
-<div class="main js-main">
+    <body class="app-body">
+    <nav class="feedback-app-navbar">
+        <div class="feedback-app-navbar-title"><img src="images/feedback-app-logo.png" class="feedback-app-logo"
+                alt="logo"> FEEDBACK <br /> APP
+        </div>
+            @yield('users')
+        <label for="navbarCheckbox" class="feedback-app-navbar-dropdown-label-close">&#10006;</label>
+    </nav>
+<div class="feedback-app-main">
     @auth
     <div class="menu-media js-menu-media"><i class="fas fa-bars"></i></div>
     @endauth
-    @yield('content')
+    <div class="profile-all-forms-container">
+        @yield('content')
+    </div>
 </div>
     @yield('script')
     </body>
